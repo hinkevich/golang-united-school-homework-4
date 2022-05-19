389e5ceafe2b755d895f1272a64f3abbb9e5100f
hinkevich <hinkevich@gmail.com>
2022-05-19 17:43:37 +0300

    add changes

--- string_sum.go
+++ string_sum.go
@@ -53,11 +53,11 @@ func StringSum(input string) (output string, err error) {
 		if len(subStringsArray) == 2 {
 			numberOne, err = strconv.Atoi(strings.TrimSpace(subStringsArray[0]))
 			if err != nil {
-				return "", err.(*strconv.NumError)
+				return "", fmt.Errorf("[StringSum] level 1 error: %w", err.(*strconv.NumError))
 			}
 			numberTwo, _ = strconv.Atoi(strings.TrimSpace(subStringsArray[1]))
 			if err != nil {
-				return "", err.(*strconv.NumError)
+				return "", fmt.Errorf("[StringSum] level 1 error: %w", err.(*strconv.NumError))
 			}
 
 			return strconv.Itoa(numberOne + numberTwo), nil
@@ -78,11 +78,11 @@ func StringSum(input string) (output string, err error) {
 		if len(subStringsArray) == 2 {
 			numberOne, err = strconv.Atoi(strings.TrimSpace(subStringsArray[0]))
 			if err != nil {
-				return "", err.(*strconv.NumError)
+				return "", fmt.Errorf("[StringSum] level 1 error: %w", err.(*strconv.NumError))
 			}
 			numberTwo, err = strconv.Atoi(strings.TrimSpace(subStringsArray[1]))
 			if err != nil {
-				return "", err.(*strconv.NumError)
+				return "", fmt.Errorf("[StringSum] level 1 error: %w", err.(*strconv.NumError))
 			}
 
 		}
